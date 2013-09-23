40059df9fdaaaa66ec8392c9add928b7bd5a9f57
ben <benoit.daffos@gmail.com>
2013-09-23 12:16:23 +0200

    url params

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,15 @@
 /* Controllers */
 
-function KarmaShowCtrl($scope, $http, $log) {
+function KarmaShowCtrl($scope, $http, $log, $location) {
 
-	$http.jsonp('http://www.reddit.com/search.json?q=site%3Ayoutube.com&limit=100&sort=top&t=week&jsonp=JSON_CALLBACK')
+	var sort = ($location.search()).sort;
+	if (typeof(sort) == 'undefined') { sort = 'top'; }
+	var subreddit = ($location.search()).subreddit;
+	if (typeof(subreddit) == 'undefined') { subreddit = '*'; }
+	var time = ($location.search()).t;
+	if (typeof(time) == 'undefined') { time = 'week'; }
+
+	$http.jsonp('http://www.reddit.com/search.json?q=site%3Ayoutube.com+subreddit%3A'+subreddit+'&limit=100&sort='+sort+'&t='+time+'&jsonp=JSON_CALLBACK')
 		.success(function(data) {
 			$scope.redditjson=data;
 	});
