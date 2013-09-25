66599b3ec1bb4e0473d5f7b82896d6b0ebdb2bb1
ben <benoit.daffos@gmail.com>
2013-09-25 23:11:47 +0200

    reload videos

--- js/controllers.js
+++ js/controllers.js
@@ -2,17 +2,21 @@
 
 function KarmaShowCtrl($scope, $http, $log, $location) {
 
-	var sort = ($location.search()).sort;
-	if (typeof(sort) == 'undefined') { sort = 'top'; }
-	var subreddit = ($location.search()).subreddit;
-	if (typeof(subreddit) == 'undefined') { subreddit = '*'; }
-	var time = ($location.search()).t;
-	if (typeof(time) == 'undefined') { time = 'week'; }
+	$scope.loadData = function() {
+		var sort = ($location.search()).sort;
+		if (typeof(sort) == 'undefined') { sort = 'top'; }
+		var subreddit = ($location.search()).subreddit;
+		if (typeof(subreddit) == 'undefined') { subreddit = '*'; }
+		var time = ($location.search()).t;
+		if (typeof(time) == 'undefined') { time = 'week'; }
 
-	$http.jsonp('http://www.reddit.com/search.json?q=site%3Ayoutube.com+subreddit%3A'+subreddit+'&limit=100&sort='+sort+'&t='+time+'&jsonp=JSON_CALLBACK')
-		.success(function(data) {
-			$scope.redditjson=data;
-	});
+		$http.jsonp('http://www.reddit.com/search.json?q=site%3Ayoutube.com+subreddit%3A'+subreddit+'&limit=100&sort='+sort+'&t='+time+'&jsonp=JSON_CALLBACK')
+			.success(function(data) {
+				$scope.redditjson=data;
+			});
+	}
+
+	$scope.loadData();
 
 	$scope.$location = $location;
 }
