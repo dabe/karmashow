2cc1735b0a7b3c7dfe3441966b0582d4548a0cce
ben <benoit.daffos@gmail.com>
2013-09-25 23:43:30 +0200

    loading image

--- js/controllers.js
+++ js/controllers.js
@@ -10,9 +10,13 @@ function KarmaShowCtrl($scope, $http, $log, $location) {
 		var time = ($location.search()).t;
 		if (typeof(time) == 'undefined') { time = 'week'; }
 
+		$scope.loading = true;
+		$scope.$apply();
 		$http.jsonp('http://www.reddit.com/search.json?q=site%3Ayoutube.com+subreddit%3A'+subreddit+'&limit=100&sort='+sort+'&t='+time+'&jsonp=JSON_CALLBACK')
 			.success(function(data) {
 				$scope.redditjson=data;
+				$scope.loading=false;
+				$scope.$apply();
 			});
 	}
 
